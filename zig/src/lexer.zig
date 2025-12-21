8fea38837d02e00072a6ae0a7c4618e06b647be2
guilhermeg2k <guilhermen2k@gmail.com>
2025-12-21 02:26:33 -0300

    Improving code

--- zig/src/lexer.zig
+++ zig/src/lexer.zig
@@ -1,5 +1,3 @@
-const LexerError = error{ InvalidIndentation, TrailingWhiteSpace };
-
 const Tag = enum {
     let,
     mut,
@@ -101,96 +99,98 @@ pub const Lexer = struct {
         return Self{ .source = source };
     }
 
+    pub fn popPendingDedent(self: *Self) Token {
+        self.pending_dedents -= 1;
+        return Token.init(Tag.dedent, self.cur_index, self.cur_index);
+    }
+
+    pub fn flushIndentation(self: *Self) void {
+        self.pending_dedents = self.last_indentation_level;
+        self.last_indentation_level = 0;
+    }
+
     pub fn next(self: *Self) Token {
-        if (self.pending_dedents != 0) {
-            self.pending_dedents -= 1;
-            return Token.init(Tag.dedent, self.cur_index, self.cur_index);
-        }
-
-        if (self.cur_index == self.source.len) {
-            if (self.last_indentation_level > 0) {
-                self.pending_dedents = self.last_indentation_level - 1;
-                self.last_indentation_level = 0;
-                return Token.init(Tag.dedent, self.cur_index, self.cur_index);
+        while (true) {
+            const should_pop_dedent = self.pending_dedents != 0;
+            if (should_pop_dedent) {
+                return self.popPendingDedent();
             }
 
-            return Token.init(Tag.eof, self.cur_index, self.cur_index);
-        }
+            const is_eof = self.cur_index == self.source.len;
+            if (is_eof and self.last_indentation_level > 0) {
+                self.flushIndentation();
+                continue;
+            }
 
-        defer self.walk();
-        const start = self.cur_index;
-        self.readWhiteSpaces();
+            if (is_eof) {
+                return Token.init(Tag.eof, self.cur_index, self.cur_index);
+            }
 
-        const char = self.source[self.cur_index];
-
-        if (char == '\n') {
-            const token = self.readNewLine() catch {
-                return Token.init(Tag.err, start, self.cur_index);
-            };
-
-            if (token) |tk| {
-                return tk;
+            defer self.walk();
+            self.readWhiteSpaces();
+            const cur_char = self.source[self.cur_index];
+            switch (cur_char) {
+                '\n' => {
+                    const token = self.readNewLine();
+                    if (token) |tk| {
+                        return tk;
+                    }
+                    continue;
+                },
+                '(' => {
+                    return Token.init(Tag.l_paren, self.cur_index, self.cur_index);
+                },
+                ')' => {
+                    return Token.init(Tag.r_paren, self.cur_index, self.cur_index);
+                },
+                ':' => {
+                    return Token.init(Tag.colon, self.cur_index, self.cur_index);
+                },
+                ',' => {
+                    return Token.init(Tag.comma, self.cur_index, self.cur_index);
+                },
+                '+' => {
+                    return Token.init(Tag.plus, self.cur_index, self.cur_index);
+                },
+                '-' => {
+                    return self.readMinusSymbol();
+                },
+                '*' => {
+                    return Token.init(Tag.star, self.cur_index, self.cur_index);
+                },
+                '/' => {
+                    return Token.init(Tag.slash, self.cur_index, self.cur_index);
+                },
+                '=' => {
+                    return self.readEqualsSymbol();
+                },
+                '>' => {
+                    return self.readGreaterSymbol();
+                },
+                '<' => {
+                    return self.readLessThanSymbol();
+                },
+                '!' => {
+                    return self.readExclamationMarkSymbol();
+                },
+                'a'...'z', 'A'...'Z', '_' => {
+                    return self.readWord();
+                },
+                '0'...'9' => {
+                    return self.readNumberLiteral();
+                },
+                '\'' => {
+                    return self.readStringLiteral();
+                },
+                else => {
+                    return Token.init(Tag.err, self.cur_index, self.cur_index);
+                },
             }
         }
-
-        switch (char) {
-            '(' => {
-                return Token.init(Tag.l_paren, self.cur_index, self.cur_index);
-            },
-            ')' => {
-                return Token.init(Tag.r_paren, self.cur_index, self.cur_index);
-            },
-            ':' => {
-                return Token.init(Tag.colon, self.cur_index, self.cur_index);
-            },
-            ',' => {
-                return Token.init(Tag.comma, self.cur_index, self.cur_index);
-            },
-            '+' => {
-                return Token.init(Tag.plus, self.cur_index, self.cur_index);
-            },
-            '-' => {
-                return self.readMinusSymbol();
-            },
-            '*' => {
-                return Token.init(Tag.star, self.cur_index, self.cur_index);
-            },
-            '/' => {
-                return Token.init(Tag.slash, self.cur_index, self.cur_index);
-            },
-            '=' => {
-                return self.readEqualsSymbol();
-            },
-            '>' => {
-                return self.readGreaterSymbol();
-            },
-            '<' => {
-                return self.readLessThanSymbol();
-            },
-            '!' => {
-                return self.readExclamationMarkSymbol();
-            },
-            'a'...'z', 'A'...'Z', '_' => {
-                return self.readWord();
-            },
-            '0'...'9' => {
-                return self.readNumberLiteral();
-            },
-            '\'' => {
-                return self.readStringLiteral();
-            },
-            else => {
-                return Token.init(Tag.err, self.cur_index, self.cur_index);
-            },
-        }
-
-        return Token.init(Tag.eof, start, self.cur_index);
     }
 
     fn readExclamationMarkSymbol(self: *Self) Token {
-        const next_char = self.peek();
-
-        if (next_char == '=') {
+        if (self.peek_next() == '=') {
             const start = self.cur_index;
             self.walk();
             return Token.init(Tag.not_equals, start, self.cur_index);
@@ -200,7 +200,7 @@ pub const Lexer = struct {
     }
 
     fn readMinusSymbol(self: *Self) Token {
-        const next_char = self.peek();
+        const next_char = self.peek_next();
 
         if (next_char == '>') {
             const start = self.cur_index;
@@ -212,7 +212,7 @@ pub const Lexer = struct {
     }
 
     fn readGreaterSymbol(self: *Self) Token {
-        const next_char = self.peek();
+        const next_char = self.peek_next();
 
         if (next_char == '=') {
             const start = self.cur_index;
@@ -224,7 +224,7 @@ pub const Lexer = struct {
     }
 
     fn readLessThanSymbol(self: *Self) Token {
-        const next_char = self.peek();
+        const next_char = self.peek_next();
 
         if (next_char == '=') {
             const start = self.cur_index;
@@ -236,7 +236,7 @@ pub const Lexer = struct {
     }
 
     fn readEqualsSymbol(self: *Self) Token {
-        const next_char = self.peek();
+        const next_char = self.peek_next();
 
         if (next_char == '=') {
             const start = self.cur_index;
@@ -255,8 +255,9 @@ pub const Lexer = struct {
 
     fn readNumberLiteral(self: *Self) Token {
         const start = self.cur_index;
+
         while (self.cur_index < self.source.len) {
-            const next_char = self.peek();
+            const next_char = self.peek_next();
             if (std.ascii.isDigit(next_char) or next_char == '.') {
                 self.walk();
             } else {
@@ -269,12 +270,12 @@ pub const Lexer = struct {
 
     fn readStringLiteral(self: *Self) Token {
         const start = self.cur_index;
-        self.walk();
 
-        while (self.peek() != '\'') {
+        while (self.peek_next() != '\'') {
             self.walk();
         }
 
+        self.walk();
         return Token.init(Tag.string_literal, start, self.cur_index);
     }
 
@@ -282,7 +283,7 @@ pub const Lexer = struct {
         const start = self.cur_index;
 
         while (self.cur_index < self.source.len) {
-            const next_char = self.peek();
+            const next_char = self.peek_next();
             if (std.ascii.isAlphanumeric(next_char) or next_char == '_') {
                 self.walk();
             } else {
@@ -298,25 +299,25 @@ pub const Lexer = struct {
         return Token.init(Tag.identifier, start, self.cur_index);
     }
 
-    fn readNewLine(self: *Self) !?Token {
+    fn readNewLine(self: *Self) ?Token {
         defer self.walk();
         const start = self.cur_index;
         var spaces: usize = 0;
 
-        while (self.peek() == ' ') {
+        while (self.peek_next() == ' ') {
             spaces += 1;
             self.walk();
         }
 
         if (spaces % INDENTATION_WIDTH != 0) {
-            return LexerError.InvalidIndentation;
+            return Token.init(Tag.err, start, self.cur_index);
         }
 
-        if (self.peek() == '\n') {
+        if (self.peek_next() == '\n') {
             if (spaces > 0) {
-                return LexerError.TrailingWhiteSpace;
+                return Token.init(Tag.err, start, self.cur_index);
             } else {
-                return self.next();
+                return null;
             }
         }
 
@@ -324,7 +325,7 @@ pub const Lexer = struct {
 
         if (indentation_level > self.last_indentation_level) {
             if (indentation_level - 1 > self.last_indentation_level) {
-                return LexerError.InvalidIndentation;
+                return Token.init(Tag.err, start, self.cur_index);
             }
             self.last_indentation_level = indentation_level;
             return Token.init(Tag.indent, start, self.cur_index);
@@ -343,7 +344,7 @@ pub const Lexer = struct {
         self.cur_index += 1;
     }
 
-    fn peek(self: *Self) u8 {
+    fn peek_next(self: *Self) u8 {
         if (self.cur_index + 1 == self.source.len) {
             return 0;
         }
@@ -355,9 +356,7 @@ pub const Lexer = struct {
 pub fn main() void {
     const src =
         \\if x:
-        \\    if y:
-        \\        z
-        \\4
+        \\    return
     ;
     var lexer = Lexer.init(src);
 
