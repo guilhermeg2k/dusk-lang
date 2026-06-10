ba23457d8a4e0e2802b3e5390c863b2f28466b5d
guilhermeg2k <guilhermen2k@gmail.com>
2026-06-10 20:32:52 -0300

    vm: change to a global stack

--- src/vm.zig
+++ src/vm.zig
@@ -8,6 +8,7 @@ pub const VM = struct {
     program: *const bc.Program,
     frames: std.ArrayList(CallFrame),
     current_frame: *CallFrame,
+    stack: [16383]bc.Value,
 
     pub fn init(allocator: std.mem.Allocator, program: *const bc.Program) VM {
         return .{
@@ -15,166 +16,181 @@ pub const VM = struct {
             .frames = .empty,
             .program = program,
             .current_frame = undefined,
+            .stack = undefined,
         };
     }
 
     pub fn run(self: *Self) void {
         const main_fn = self.program.functions[self.program.main_func_index];
 
-        var frame = CallFrame{
+        self.frames.append(self.allocator, CallFrame{
             .function = &main_fn,
             .cur_inst = 0,
-            .registers = undefined,
-        };
-
-        self.current_frame = &frame;
+            .stack_start_index = 0,
+        }) catch @panic("OOM");
+        self.current_frame = &self.frames.items[self.frames.items.len - 1];
 
         const instructions = self.current_frame.function.chunk.instructions;
         const constants = self.current_frame.function.chunk.constants;
 
         while (self.current_frame.cur_inst < instructions.len) : (self.current_frame.cur_inst += 1) {
             const inst = instructions[self.current_frame.cur_inst];
+            const stack = self.stack[0..];
             switch (inst.op) {
                 .LOAD_CONST => {
-                    frame.registers[inst.a] = constants[inst.b];
+                    self.current_frame.setVar(stack, inst.a, constants[inst.b]);
                 },
                 .LOAD => {
-                    frame.registers[inst.a] = frame.registers[inst.b];
+                    self.current_frame.setVar(stack, inst.a, self.current_frame.getVar(stack, inst.b));
                 },
 
                 .I_ADD => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_int = frame.registers[inst.b].i_int + frame.registers[inst.c].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_int = self.current_frame.getVar(stack, inst.b).i_int + self.current_frame.getVar(stack, inst.c).i_int,
+                    });
                 },
                 .I_SUB => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_int = frame.registers[inst.b].i_int - frame.registers[inst.c].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_int = self.current_frame.getVar(stack, inst.b).i_int - self.current_frame.getVar(stack, inst.c).i_int,
+                    });
                 },
                 .I_MULT => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_int = frame.registers[inst.b].i_int * frame.registers[inst.c].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_int = self.current_frame.getVar(stack, inst.b).i_int * self.current_frame.getVar(stack, inst.c).i_int,
+                    });
                 },
                 .I_DIV => {
-                    frame.registers[inst.a] = bc.Value{ .i_int = @divTrunc(
-                        frame.registers[inst.b].i_int,
-                        frame.registers[inst.c].i_int,
-                    ) };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{ .i_int = @divTrunc(
+                        self.current_frame.getVar(stack, inst.b).i_int,
+                        self.current_frame.getVar(stack, inst.c).i_int,
+                    ) });
                 },
                 .I_MOD => {
-                    frame.registers[inst.a] = bc.Value{ .i_int = @mod(
-                        frame.registers[inst.b].i_int,
-                        frame.registers[inst.c].i_int,
-                    ) };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{ .i_int = @mod(
+                        self.current_frame.getVar(stack, inst.b).i_int,
+                        self.current_frame.getVar(stack, inst.c).i_int,
+                    ) });
                 },
                 .I_EQ => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_int == frame.registers[inst.c].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_int == self.current_frame.getVar(stack, inst.c).i_int,
+                    });
                 },
                 .I_NEQ => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_int != frame.registers[inst.c].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_int != self.current_frame.getVar(stack, inst.c).i_int,
+                    });
                 },
                 .I_LT => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_int < frame.registers[inst.c].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_int < self.current_frame.getVar(stack, inst.c).i_int,
+                    });
                 },
                 .I_LE => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_int <= frame.registers[inst.c].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_int <= self.current_frame.getVar(stack, inst.c).i_int,
+                    });
                 },
                 .I_GT => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_int > frame.registers[inst.c].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_int > self.current_frame.getVar(stack, inst.c).i_int,
+                    });
                 },
                 .I_GE => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_int >= frame.registers[inst.c].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_int >= self.current_frame.getVar(stack, inst.c).i_int,
+                    });
                 },
                 .I_NEG => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_int = -frame.registers[inst.b].i_int,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_int = -self.current_frame.getVar(stack, inst.b).i_int,
+                    });
                 },
 
                 .F_ADD => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_float = frame.registers[inst.b].i_float + frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_float = self.current_frame.getVar(stack, inst.b).i_float + self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_SUB => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_float = frame.registers[inst.b].i_float - frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_float = self.current_frame.getVar(stack, inst.b).i_float - self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_MULT => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_float = frame.registers[inst.b].i_float * frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_float = self.current_frame.getVar(stack, inst.b).i_float * self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_DIV => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_float = frame.registers[inst.b].i_float / frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_float = self.current_frame.getVar(stack, inst.b).i_float / self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_MOD => {
-                    frame.registers[inst.a] = bc.Value{ .i_float = @mod(
-                        frame.registers[inst.b].i_float,
-                        frame.registers[inst.c].i_float,
-                    ) };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{ .i_float = @mod(
+                        self.current_frame.getVar(stack, inst.b).i_float,
+                        self.current_frame.getVar(stack, inst.c).i_float,
+                    ) });
                 },
                 .F_EQ => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_float == frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_float == self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_NEQ => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_float != frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_float != self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_LT => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_float < frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_float < self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_LE => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_float <= frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_float <= self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_GT => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_float > frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_float > self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_GE => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_bool = frame.registers[inst.b].i_float >= frame.registers[inst.c].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_bool = self.current_frame.getVar(stack, inst.b).i_float >= self.current_frame.getVar(stack, inst.c).i_float,
+                    });
                 },
                 .F_NEG => {
-                    frame.registers[inst.a] = bc.Value{
-                        .i_float = -frame.registers[inst.b].i_float,
-                    };
+                    self.current_frame.setVar(stack, inst.a, bc.Value{
+                        .i_float = -self.current_frame.getVar(stack, inst.b).i_float,
+                    });
                 },
                 else => {},
             }
         }
-        std.debug.print("Result in R: {any}\n", .{self.current_frame.registers[0..14]});
+        std.debug.print("Result in R: {any}\n", .{self.stack[self.current_frame.stack_start_index .. self.current_frame.stack_start_index + 14]});
     }
 };
 
 const CallFrame = struct {
+    const Self = @This();
+
     function: *const bc.Function,
-    cur_inst: u32,
-    registers: [256]bc.Value,
+    cur_inst: usize,
+    stack_start_index: usize,
+
+    pub fn getStackIndex(self: *Self, index: u8) usize {
+        return self.stack_start_index + index;
+    }
+
+    pub fn getVar(self: *Self, stack: []bc.Value, index: u8) bc.Value {
+        return stack[self.getStackIndex(index)];
+    }
+
+    pub fn setVar(self: *Self, stack: []bc.Value, index: u8, value: bc.Value) void {
+        stack[self.getStackIndex(index)] = value;
+    }
 };
